05b75919825114eeb532276be72334516fbc1e0c
Oleg Anuchin <oleg.anuchin@gmail.com>
2021-11-14 04:01:59 +0700

    WIP Scenes

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const bot = app.get(getBotToken());
   bot.use(Telegraf.log());
-  console.log({bot});
+  // console.log({bot});
   
   await app.listen(3000);
   console.log('App started at port 3000');
--- backend/src/telegram/telegram.service.ts
+++ backend/src/telegram/telegram.service.ts
@@ -1,8 +1,9 @@
-import { Telegraf, Markup, session } from 'telegraf';
+import { Telegraf, Markup, session, Scenes } from 'telegraf';
 import { Injectable } from "@nestjs/common";
 
 import { Context } from "../entities/interfaces";
 import { InjectBot } from 'nestjs-telegraf';
+import { hotPurchasesScene } from './scenes/hotPurchasesScene'
 // import { AuctionService } from 'src/auction/auction.service'; 
 
 @Injectable()
@@ -13,12 +14,17 @@ export class TelegramService {
   ) {
 
     this.bot.use(session())
+    const stage = new Scenes.Stage<Scenes.SceneContext>([hotPurchasesScene], {
+      ttl: 10,
+    })
+    bot.use(stage.middleware())
     this.bot.start((ctx) => ctx.reply(this.getMenu(ctx)))
 
     this.bot.action('HOT_PURCHASE', (ctx) => this.showHotPurchase({ ctx, index: null }))
-    // this.bot.on('sticker', (ctx) => ctx.reply('👍'))
-    // this.bot.hears('auth', (ctx) => ctx.reply('Hey there'))
-
+    // this.bot.action('HOT_PURCHASE_BID', (ctx) => this.bidPurchase({ ctx }))
+    // this.bot.action('HOT_PURCHASE_LATER', (ctx) => this.laterPurchase({ ctx, index: null }))
+    // this.bot.action('HOT_PURCHASE_AUTO_BID', (ctx) => this.autobidPurchase({ ctx, index: null }))
+    // this.bot.action('HOT_PURCHASE_NOT_INTRESTED', (ctx) => this.processPurchase({ ctx, index: null }))
   }
 
   getMenu(ctx: any): string {
@@ -36,46 +42,21 @@ export class TelegramService {
   }
 
   showHotPurchase({ ctx, index }) {
-    let currentPurchase = null;
-    if (index === null) {
-      ctx.session = { hotPurchases: FAKE_PURCHASES }
-      index = 0
-    }
+    return ctx.scene.enter('HOT_PURCHASES_SCENE')
+  }
 
-    const count = ctx.session.hotPurchases.length
-    if (count === 0) {
-      return ctx.reply('Пусто :-(')
+  bidPurchase() {
 
-    } else {
-      currentPurchase = ctx.session.hotPurchases[index]
-    }
+  }
 
-    ctx.session.currentHotPurchase = index
+  laterPurchase() {
 
-    const { itemName, quantity, beforeEnd, currentPrice, customerName, currentPriceOneItem, id } = currentPurchase
+  }
+  autobidPurchase() {
 
-    return ctx.reply(`
-Горящие закупки: ${index + 1} из ${count}
+  }
 
-Номер: ${id}
-
-${itemName}, в  кол. ${quantity}
-До завершения: ${beforeEnd}
-
-Текущая цена: ${currentPrice} р., за единицу ${currentPriceOneItem} р.
-
-Заказчик: ${customerName}`,
-      Markup.inlineKeyboard([
-        Markup.button.callback('Сделать ставку', 'HOT_PURCHASE_BID'),
-        Markup.button.callback('Автоматические ставки', 'HOT_PURCHASE_AUTO_BID'),
-        Markup.button.callback('Посмотрю позже', 'HOT_PURCHASE_LATER'),
-        Markup.button.callback('Не интересно', 'HOT_PURCHASE_NOT_INTRESTED'),
-      ], { wrap: () => true }))
-
-
-
-
-
+  processPurchase() {
 
   }
 
@@ -94,10 +75,4 @@ ${itemName}, в  кол. ${quantity}
   }
 }
 
-const FAKE_PURCHASES = [
-  { itemName: 'Автомат Калашникова', quantity: 100, beforeEnd: '1 ч. 5 м.', currentPrice: 100, customerName: 'Управление образованием', currentPriceOneItem: 1, id: '36к8268563', isProcessed: false },
-  { itemName: 'Автомат Бориса', quantity: 100, beforeEnd: '1 ч. 5 м.', currentPrice: 100, customerName: 'Управление образованием', currentPriceOneItem: 1, id: '36к8268563', isProcessed: false },
-  { itemName: 'Автомат Никиты', quantity: 100, beforeEnd: '1 ч. 5 м.', currentPrice: 100, customerName: 'Управление образованием', currentPriceOneItem: 1, id: '36к8268563', isProcessed: false },
-  { itemName: 'Автомат Олега', quantity: 100, beforeEnd: '1 ч. 5 м.', currentPrice: 100, customerName: 'Управление образованием', currentPriceOneItem: 1, id: '36к8268563', isProcessed: false },
-  { itemName: 'Автомат Влада', quantity: 100, beforeEnd: '1 ч. 5 м.', currentPrice: 100, customerName: 'Управление образованием', currentPriceOneItem: 1, id: '36к8268563', isProcessed: false },
-]
+
